9e264aa5013ece49a5235ce5cfedacf4bb0915a5
Alexey Kalinin <alkaline@yandex-team.ru>
2014-06-17 12:27:59 +0400

    функция-обертка

--- DirectSupToDirect.js
+++ DirectSupToDirect.js
@@ -1,96 +1,98 @@
-var rootPath = 'https://jira.yandex-team.ru/rest',
-	apiPath = rootPath + '/api/latest',
-	gadgetPath = rootPath + '/gadget/latest',
-	webPath = 'https://jira.yandex-team.ru/browse/',
-	issueKey = document.location.pathname.match(/browse\/(.+)/)[1];
+(function() {
+	var rootPath = 'https://jira.yandex-team.ru/rest',
+		apiPath = rootPath + '/api/latest',
+		gadgetPath = rootPath + '/gadget/latest',
+		webPath = 'https://jira.yandex-team.ru/browse/',
+		issueKey = document.location.pathname.match(/browse\/(.+)/)[1];
 
-if(!issueKey) return;
+	if(!issueKey) return;
 
-function microAjax(url, callback) {
-	var req = (function() {
-			if (window.ActiveXObject)
-				return new ActiveXObject('Microsoft.XMLHTTP');
-			else if (window.XMLHttpRequest)
-				return new XMLHttpRequest();
-			return false;
-		}()),
-		postBody = (arguments[2] || "");
-	
-	req.onreadystatechange = function (object) {
-		if (req.readyState == 4) {
-			callback(req.responseText);
+	function microAjax(url, callback) {
+		var req = (function() {
+				if (window.ActiveXObject)
+					return new ActiveXObject('Microsoft.XMLHTTP');
+				else if (window.XMLHttpRequest)
+					return new XMLHttpRequest();
+				return false;
+			}()),
+			postBody = (arguments[2] || "");
+		
+		req.onreadystatechange = function (object) {
+			if (req.readyState == 4) {
+				callback(req.responseText);
+			}
+		};
+
+		if (postBody !== "") {
+			req.open("POST", url, true);
+			// req.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+			req.setRequestHeader('Content-Type', 'application/json');
+		} else {
+			req.open("GET", url, true);
 		}
-	};
 
-	if (postBody !== "") {
-		req.open("POST", url, true);
-		// req.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-		req.setRequestHeader('Content-Type', 'application/json');
-	} else {
-		req.open("GET", url, true);
+		req.send(postBody);
 	}
 
-	req.send(postBody);
-}
+	var newIssueParams = {
+		fields: {}
+	};
 
-var newIssueParams = {
-	fields: {}
-};
+	microAjax(gadgetPath + '/currentUser', function(response) {
+		var user = JSON.parse(response);
 
-microAjax(gadgetPath + '/currentUser', function(response) {
-	var user = JSON.parse(response);
+		newIssueParams.fields.assignee = newIssueParams.fields.reporter = { name : user.username };
 
-	newIssueParams.fields.assignee = newIssueParams.fields.reporter = { name : user.username };
+		microAjax(apiPath + '/issue/' + issueKey + '?fields=summary,components,labels', function(response) {
+			var originalIssueData = JSON.parse(response);
 
-	microAjax(apiPath + '/issue/' + issueKey + '?fields=summary,components,labels', function(response) {
-		var originalIssueData = JSON.parse(response);
+			newIssueParams.fields.summary = issueKey + ':' + originalIssueData.fields.summary;
+			
+			// newIssueParams.fields.components = (originalIssueData.fields.components || []).map(function(component) { return { id: component.id }});
+			newIssueParams.fields.labels = originalIssueData.fields.labels;
 
-		newIssueParams.fields.summary = issueKey + ':' + originalIssueData.fields.summary;
-		
-		// newIssueParams.fields.components = (originalIssueData.fields.components || []).map(function(component) { return { id: component.id }});
-		newIssueParams.fields.labels = originalIssueData.fields.labels;
+			microAjax(apiPath + '/issue/createmeta?projectKeys=DIRECT&issuetypeNames=Bug', function(response) {
+				var createMeta = JSON.parse(response);
 
-		microAjax(apiPath + '/issue/createmeta?projectKeys=DIRECT&issuetypeNames=Bug', function(response) {
-			var createMeta = JSON.parse(response);
+				newIssueParams.fields.project = { id: createMeta.projects[0].id };
+				newIssueParams.fields.issuetype = { id: createMeta.projects[0].issuetypes[0].id };
 
-			newIssueParams.fields.project = { id: createMeta.projects[0].id };
-			newIssueParams.fields.issuetype = { id: createMeta.projects[0].issuetypes[0].id };
+				microAjax(apiPath + '/project/DIRECT/components', function(response) {
+					var components = JSON.parse(response),
+						componentId;
 
-			microAjax(apiPath + '/project/DIRECT/components', function(response) {
-				var components = JSON.parse(response),
-					componentId;
+					components.some(function(component) {
+						if(/Поддержка/.test(component.name)) {
+							componentId = component.id;
+							return true;
+						}
 
-				components.some(function(component) {
-					if(/Поддержка/.test(component.name)) {
-						componentId = component.id;
-						return true;
-					}
+						return false;
+					})
 
-					return false;
-				})
+					if(!componentId) return;
 
-				if(!componentId) return;
+					newIssueParams.fields.components = [{ id: componentId }];
 
-				newIssueParams.fields.components = [{ id: componentId }];
-
-				microAjax(apiPath + '/issue', function(response) {
-					var newIssueData = JSON.parse(response),
-						linkData = {
-							type: { name: 'Relation' },
-						    inwardIssue: { key: newIssueData.key },
-						    outwardIssue: { key: originalIssueData.key }
-						};
-
-					microAjax(apiPath + '/issue', function() {
-						linkData.inwardIssue.key = originalIssueData.key;
-						linkData.outwardIssue.key = newIssueData.key;
+					microAjax(apiPath + '/issue', function(response) {
+						var newIssueData = JSON.parse(response),
+							linkData = {
+								type: { name: 'Relation' },
+							    inwardIssue: { key: newIssueData.key },
+							    outwardIssue: { key: originalIssueData.key }
+							};
 
 						microAjax(apiPath + '/issue', function() {
-							document.location = webPath + newIssueData.key;		
+							linkData.inwardIssue.key = originalIssueData.key;
+							linkData.outwardIssue.key = newIssueData.key;
+
+							microAjax(apiPath + '/issue', function() {
+								document.location = webPath + newIssueData.key;		
+							}, JSON.stringify(linkData));
 						}, JSON.stringify(linkData));
-					}, JSON.stringify(linkData));
-				}, JSON.stringify(newIssueParams));
-			});
-		});	
+					}, JSON.stringify(newIssueParams));
+				});
+			});	
+		});
 	});
-});
+}())
